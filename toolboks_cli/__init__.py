442a92a003c2fab0c3d445909c719872132f9add
Mikael Tranbom <mikael@tranbom.io>
2022-12-08 20:22:13 +0100

    Update line to ignore flake8 error

--- toolboks_cli/__init__.py
+++ toolboks_cli/__init__.py
@@ -15,4 +15,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from toolboks_cli._version import __version__
+from toolboks_cli._version import __version__  # noqa: F401
